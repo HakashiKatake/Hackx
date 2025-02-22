33fe49b7e276cd580b5bfd69ec563b41c0401023
Rohan Vashisht <rohanprogrammer1@gmail.com>
2025-02-23 00:15:24 +0530

    ok

---
+++ Site/fitchecker/src/app/3d-preview/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ThreeDPreview: React.FC = () => {
+    return (
+        <div>
+            <h1>3D Preview</h1>
+            {/* Add your 3D preview component or logic here */}
+        </div>
+    );
+};
+
+export default ThreeDPreview;
