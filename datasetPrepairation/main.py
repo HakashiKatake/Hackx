aa83c1faad4e6d570f26a9500eda13acd337c97b
Rohan Vashisht <rohanprogrammer1@gmail.com>
2025-02-22 18:27:48 +0530

    ok

--- datasetPrepairation/main.py
+++ datasetPrepairation/main.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 
+
 # Load dataset
 df = pd.read_csv("datasetPrepairation/rawDatasets/Clothes-Size-Prediction.csv")  # Update with the actual filename
 
