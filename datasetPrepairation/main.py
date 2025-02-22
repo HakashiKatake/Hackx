2cc7757f68ea4899641fe47aa4ff831e6ebbdd53
Rohan Vashisht <rohanprogrammer1@gmail.com>
2025-02-22 13:55:16 +0530

    ok

--- datasetPrepairation/main.py
+++ datasetPrepairation/main.py
@@ -1,25 +1,36 @@
 from datasets import load_dataset
 
+import pandas as pd
 
-dataset = load_dataset("csv", data_files="./datasetPrepairation/rawDatasets/bdm.csv")
+dataset = load_dataset(
+    "csv", data_files="./datasetPrepairation/rawDatasets/bdm.csv", split="train"
+)
 
 
 # map over the dataset and replace the ShoulderWidth in inches to centimeters
 def convert_to_cm(example):
-    example["ShoulderWidth"] = example["ShoulderWidth"] * 2.54
-    example["ChestWidth"] = example["ChestWidth"] * 2.54
-    example["Belly"] = example["Belly"] * 2.54
-    example["Waist"] = example["Waist"] * 2.54
-    example["Hips"] = example["Hips"] * 2.54
-    example["ArmLength"] = example["ArmLength"] * 2.54
-    example["WaistToKnee"] = example["WaistToKnee"] * 2.54
-    example["ShoulderToWaist"] = example["ShoulderToWaist"] * 2.54
-    example["LegLength"] = example["LegLength"] * 2.54
-    example["TotalHeight"] = example["TotalHeight"] * 2.54
+    #  limit to 2 deciaml places
+
+    example["ShoulderWidth"] = round(example["ShoulderWidth"] * 2.54, 2)
+    example["ChestWidth"] = round(example["ChestWidth"] * 2.54, 2)
+    example["Belly"] = round(example["Belly"] * 2.54, 2)
+    example["Waist"] = round(example["Waist"] * 2.54, 2)
+    example["Hips"] = round(example["Hips"] * 2.54, 2)
+    example["ArmLength"] = round(example["ArmLength"] * 2.54, 2)
+    example["WaistToKnee"] = round(example["WaistToKnee"] * 2.54, 2)
+    example["ShoulderToWaist"] = round(example["ShoulderToWaist"] * 2.54, 2)
+    example["LegLength"] = round(example["LegLength"] * 2.54, 2)
+    example["HeadCircumference"] = round(example["HeadCircumference"] * 2.54, 2)
+    example["TotalHeight"] = round(example["TotalHeight"] * 2.54, 2)
     return example
 
 
 dataset = dataset.map(convert_to_cm)
+
+# convert dataset to pandas dataframe
+dataset = pd.DataFrame(dataset)
+# save it on disk as csv
+dataset.to_csv("./datasetPrepairation/processedDatasets/bdm.csv", index=False)
 
 print(dataset)
 
