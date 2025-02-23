d914edbeaf80b72ae22e8fe25e2271fa47f7139f
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-02-23 10:26:14 +0530

    dock update

--- Site/fitchecker/src/components/DockWrapper.tsx
+++ Site/fitchecker/src/components/DockWrapper.tsx
@@ -5,11 +5,14 @@ import React from 'react';
 import Dock from '@/components/Dock/Dock'; // Adjust path if necessary
 import { VscHome, VscArchive} from 'react-icons/vsc';
 import { FaRobot } from "react-icons/fa";
+import { FaBrain } from "react-icons/fa";
 
 const dockItems = [
   { icon: <VscHome size={18} />, label: "Home", onClick: () => window.location.href = "/" },
+  { icon: <FaBrain size={18} />, label: "Start Measurement", onClick: () => window.location.href = "/results" },
   { icon: <VscArchive size={18} />, label: "Ar Archive", onClick: () => window.location.href = "/ar-preview" },
   { icon: <FaRobot size={18} />, label: "Chat with Swag AI", onClick: () => window.location.href = "/help" },
+  
 ];
 
 export default function DockWrapper() {
