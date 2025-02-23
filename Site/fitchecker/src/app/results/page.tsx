55b63ee939427f5e949cbe927d5aca203b7d14dd
Rohan Vashisht <rohanprogrammer1@gmail.com>
2025-02-23 06:01:58 +0530

    ok

--- Site/fitchecker/src/app/results/page.tsx
+++ Site/fitchecker/src/app/results/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const page = () => {
+const Page = () => {
   return (
-    <iframe className='w-full h-full' src="https://rohanvashisht-hackx.hf.space/?"></iframe>
-  )
-}
+    <>
+      <nav className="h-[100px] w-full bg-[3f3f46] text-white flex items-center justify-between px-6 shadow-lg">
+        <h1 className="text-2xl font-bold">Get Your Size</h1>
+        <ul className="flex space-x-6">
+          <li className="hover:text-gray-400 cursor-pointer"><a href="https://rohanvashisht-hackx.hf.space/">Open in Tab</a></li>
+        </ul>
+      </nav>
+      <embed className="w-screen h-[calc(100vh-100px)]" src="https://rohanvashisht-hackx.hf.space/"></embed>
+    </>
+  );
+};
 
-export default page
+export default Page;
